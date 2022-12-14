2fe060c375d410ca39bac88fc3971c566b330b3c
galiano <edigaliano@gmail.com>
2022-12-14 14:41:20 -0300

    Setup do Projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+const PORT = 8080;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/agendamento")
+
+app.get("/", (req, res) => {
+    res.send("Olá!");
+})
+
+app.listen(PORT, () => {console.log("Server Running on Port", PORT)});
